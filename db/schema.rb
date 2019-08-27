1e986d867b97cae310fab82cb158215665e4e0df
junpei <junpei12131018@gmail.com>
2019-08-27 04:47:40 +0000

    Add Carts Orders tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_040952) do
+ActiveRecord::Schema.define(version: 2019_08_27_044519) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2019_08_27_040952) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -57,6 +65,19 @@ ActiveRecord::Schema.define(version: 2019_08_27_040952) do
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
     t.index [nil], name: "index_customers_on_deleted_at"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "payment"
+    t.string "shipping_address"
+    t.string "delivery_status"
+    t.integer "subtotal_amount"
+    t.integer "total_amount"
+    t.integer "postage"
+    t.integer "consumption_tax"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
